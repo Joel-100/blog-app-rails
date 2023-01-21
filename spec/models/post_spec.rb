89a312bb67d78f2a868924e80ab58e2ea2626277
Joel-100 <joelkugonza@gmail.com>
2023-01-21 20:54:57 +0300

    fix-post-and-like.rb

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it 'should be valid' do
+  it 'should not be valid' do
     post = Post.create(title: nil)
     expect(post).to_not be_valid
   end
