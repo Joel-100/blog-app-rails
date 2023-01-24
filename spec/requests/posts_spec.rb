de22aa3db529a871eaace7d1c686fa74c7490d02
Joel-100 <joelkugonza@gmail.com>
2023-01-24 14:43:08 +0300

    add-controller-test

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,17 +1,35 @@
 require 'rails_helper'
 
-Rspec.describe 'Posts', type: :request do
-  describe 'GET /index' do
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /users/:id/posts' do
+    before(:example) { get '/users/12/posts' }
+
     it 'returns http success' do
-      get '/posts/index'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the :index template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'display header in the body response' do
+      expect(response.body).to include('Here is a list of posts for a given user')
     end
   end
 
-  describe 'GET /show' do
+  describe 'GET /users/:id/posts/:id' do
+    before(:example) { get '/users/12/posts/24' }
+
     it 'returns http success' do
-      get '/posts/show'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the :show template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'display header in the body response' do
+      expect(response.body).to include('Here is a single post from list of posts for a given user')
     end
   end
 end
