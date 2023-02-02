fd80a752a4df4d4deb78eca942fc9b958e30f485
Joel-100 <joelkugonza@gmail.com>
2023-02-02 10:49:08 +0300

    Remove current_user method in ApplicationController and Install Devise gem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,32 +1,31 @@
-class PostsController < ApplicationController
-  def index
-    @user = User.find(params[:user_id])
-    @posts = @user.posts.includes(:comments)
-  end
+# class PostsController < ApplicationController
+#   def index
+#     @user = User.find(params[:user_id])
+#   end
 
-  def show
-    @post = Post.includes(:comments, :likes).find(params[:id])
-  end
+#   def show
+#     @post = Post.includes(:comments, :likes).find(params[:id])
+#   end
 
-  def new
-    @post = Post.new
-  end
+#   def new
+#     @post = Post.new
+#   end
 
-  def create
-    @post = Post.new(post_params)
-    @post.user_id = current_user.id
-    if @post.save
-      flash[:success] = 'Your Post has benn created successfully'
-      redirect_to user_post_path(current_user, @post)
-    else
-      flash.now[:error] = 'Error message: Post was not created !'
-      render :new
-    end
-  end
+#   def create
+#     @post = current_user.posts.new(post_params)
 
-  private
+#     if @post.save
+#       flash[:success] = 'Your Post has benn created successfully'
+#       redirect_to user_post_path(current_user, @post)
+#     else
+#       flash.now[:error] = 'Error message: Post was not created !'
+#       render :new
+#     end
+#   end
 
-  def post_params
-    params.require(:post).permit(:title, :text)
-  end
-end
+#   private
+
+#   def post_params
+#     params.require(:post).permit(:title, :text)
+#   end
+# end
